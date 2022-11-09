a6776b0b340687bbdcd932ece6a2e00851428c25
Z1gZ0g <info@Z1gZ0g.ga>
2022-11-09 15:54:41 +0330

    implement animal

---
+++ src/logic/animal/Animal.java
@@ -0,0 +1,24 @@
+package logic.animal;
+
+import logic.Health;
+import logic.Movement;
+import logic.mechanism.AttackMechanism;
+import logic.mechanism.DefenseMechanism;
+import logic.eatable.Substance;
+
+import java.util.ArrayList;
+
+public abstract class Animal {
+    private String id;
+
+    private String name;
+
+    private ArrayList<AttackMechanism> attackMechanisms;
+    private ArrayList<DefenseMechanism> defenseMechanisms;
+    private Health health;
+    private Movement walk;
+    private Movement run;
+
+
+    public abstract void eat(ArrayList<Substance> substances);
+}
---
+++ src/logic/animal/WildAnimal.java
@@ -0,0 +1,12 @@
+package logic.animal;
+
+import logic.eatable.Substance;
+
+import java.util.ArrayList;
+
+public class WildAnimal extends Animal {
+    @Override
+    public void eat(ArrayList<Substance> substances) {
+
+    }
+}
